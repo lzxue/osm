d0ad3b41bbe09ff0f322523b6321a6113974bffb
@thinkinggis <lzx199065@gmail.com>
2024-06-14 08:05:25 +0000

    feat: 初始化项目

---
+++ geo.py
@@ -0,0 +1,157 @@
+from typing import Tuple, Optional
+from osmnx.geocoder import geocode
+from geopandas import GeoDataFrame
+import pandas as pd
+from pandas import DataFrame
+from shapely.geometry import Polygon, Point, box
+from streamlit_l7 import l7
+
+
+class GeoCodingError(Exception):
+    pass
+
+
+def validate_coordinates(lat: float, lon: float) -> None:
+    if lat < -90 or lat > 90 or lon < -180 or lon > 180:
+        raise ValueError(
+            "longitude (-90 to 90) and latitude (-180 to 180) coordinates "
+            "are not within valid ranges."
+        )
+
+
+def get_aoi(
+    address: Optional[str] = None,
+    coordinates: Optional[Tuple[float, float]] = None,
+    radius: int = 1000,
+    rectangular: bool = False,
+) -> Polygon:
+    """
+    Gets round or rectangular shapely Polygon in in 4326 from input address or coordinates.
+
+    Args:
+        address: Address string
+        coordinates: lat, lon
+        radius: Radius in meter
+        rectangular: Optionally return aoi as rectangular polygon, default False.
+
+    Returns:
+        shapely Polygon in 4326 crs
+    """
+    if address is not None:
+        if coordinates is not None:
+            raise ValueError(
+                "Both address and latlon coordinates were provided, please "
+                "select only one!"
+            )
+        try:
+            lat, lon = geocode(address)
+        except ValueError as e:
+            raise GeoCodingError(f"Could not geocode address '{address}'") from e
+    else:
+        lat, lon = coordinates  # type: ignore
+    validate_coordinates(lat, lon)
+
+    df = GeoDataFrame(
+        DataFrame([0], columns=["id"]), crs="EPSG:4326", geometry=[Point(lon, lat)]
+    )
+    utm_crs = df.estimate_utm_crs()
+    df = df.to_crs(utm_crs)
+    df.geometry = df.geometry.buffer(radius)
+    df = df.to_crs(crs=4326)
+    poly = df.iloc[0].geometry
+
+    if rectangular:
+        poly = box(*poly.bounds)
+
+    return poly
+
+
+def explode_multigeometries(df: GeoDataFrame) -> GeoDataFrame:
+    """
+    Explode all multi geometries in a geodataframe into individual polygon geometries.
+    Adds exploded polygons as rows at the end of the geodataframe and resets its index.
+    Args:
+        df: Input GeoDataFrame
+    """
+    mask = df.geom_type.isin(["MultiPolygon", "MultiLineString", "MultiPoint"])
+    outdf = df[~mask]
+    df_multi = df[mask]
+    for _, row in df_multi.iterrows():
+        df_temp = GeoDataFrame(
+            pd.DataFrame.from_records([row.to_dict()] * len(row.geometry.geoms)), crs="EPSG:4326"
+        )
+        df_temp.geometry = list(row.geometry.geoms)
+        outdf = GeoDataFrame(pd.concat([outdf, df_temp], ignore_index=True), crs="EPSG:4326")
+
+    outdf = outdf.reset_index(drop=True)
+    return outdf
+
+def amap_geocode(address: str):
+    """
+    Geocode address with amap api.
+    Args:
+        address: Address string
+    Returns:
+        shapely Point in 4326 crs
+    """
+    import requests
+    import json
+    params = {
+        "key": "d949994fb93bc4820270a9103a11e19f",
+        "address": address,
+    }
+    response = requests.get('https://restapi.amap.com/v3/geocode/geo', params=params)
+    data = json.loads(response.text)
+    addr = data["geocodes"][0]
+    location = addr['location']
+    formatted_address = addr['formatted_address']
+    lon, lat = location.split(",")
+    point = (float(lon), float(lat))
+    return (point,formatted_address)
+
+def records_from_geo_interface(data):
+    """Un-nest data from object implementing __geo_interface__ standard"""
+    flattened_records = []
+    for d in data.__geo_interface__.get("features"):
+        record = d.get("properties", {})
+        geom = d.get("geometry", {})
+        record["geometry"] = geom
+        flattened_records.append(record)
+    return flattened_records
+
+
+def renderChoroplethMap(geoData,line_color,background_color) :
+    options = {
+      "mapType": 'Map',
+      "mapOptions": {
+        "center": [120.210792, 30.246026],
+        "zoom": 16,
+      },
+      "layers": [ 
+                    
+            {
+              "type":'line',
+              "source":{
+                  "data":geoData,
+                   "parser": {"type": 'json', "geometry": 'geometry'},
+              },
+              "autoFit": True,
+              "shape": 'simple',
+              "size": 1,
+              "color":line_color,
+        
+            }
+      ],
+         
+      "controls": [
+          {
+          "type": 'zoom',
+          },{
+            "type": 'scale',
+          },{
+            "type": 'fullscreen'
+          }
+      ],
+  }
+    l7(options=options, style={"height": 400,"background":background_color}, key="streamlit-l7")
+   
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,44 @@
-import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import osmnx as osm
+from geo import get_aoi,amap_geocode,records_from_geo_interface,renderChoroplethMap
 
-"""
-# Welcome to Streamlit!
+st.set_page_config(
+    page_title="OSM Map",
+    page_icon=":smiley:",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+col1, col2, col3 = st.columns(3)
+background_color = col1.color_picker("选择背景颜色", "#fff")
+line_color = col1.color_picker("选择路网颜色", "#000")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+radius = col2.slider("选择一个值", min_value=500, max_value=10000, value=1000, step=100)
+address=col3.text_input("请输入地址", "Hangzhou, China")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+addressGeo= get_aoi(address=address,radius=radius)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+point,formatted_address = amap_geocode(address)
+lng,lat = point
+st.write(formatted_address,point)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+fivekm_buffer_nw = osm.features_from_point((lat,lng),tags={
+       "highway": [
+            "motorway",
+            "trunk",
+            "primary",
+            "secondary",
+            "tertiary",
+            "cycleway",
+            "residential",
+            "service",
+            "unclassified",
+            "footway",
+            "motorway_link",
+        ],
+        # "natural": ["water", "bay"],
+        # "place": ["sea"],
+    },dist=radius)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+renderChoroplethMap(records_from_geo_interface(fivekm_buffer_nw),line_color,background_color)
